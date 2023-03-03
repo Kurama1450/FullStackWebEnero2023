5392dcd1a56487114c86d9f57c0ad7acb09f046d
Kurama1450 <nestorcostaorts@gmail.com>
2023-03-03 09:19:59 +0100

    Update js

--- JS/proyectoJS/proyectoJS.js
+++ JS/proyectoJS/proyectoJS.js
@@ -48,188 +48,184 @@ UN BOTON AÑADIR. AÑADA 1 NUEVO A LA ESTRUCTURA.*/
 	}*/
 	
 	function cambiarEstructura(){
+		let f=document.formu;
+		let tablaM=f.tabla.value;
+		let cantidad=f.cantidad.value;
 		
-		let f = document.formu;
-		let tablaM = f.tabla.value;
-		let cantidad = f.cantidad.value;
 		
 		if(this.id=="tabla"){
 			hacerLista(tablaM,cantidad);
 			document.formu.estructura.value="LISTA";
-		}	
-		else if(this.id == "lista"){
+		}
+		else if(this.id=="lista"){
 			hacerDesplegable(tablaM,cantidad);
 			document.formu.estructura.value="DESPLEGABLE";
-		}	
-		else if(this.id="desplegable"){
-			hacerParrafo(tablaM,cantidad);
+		}
+		else if(this.id=="desplegable"){
+			hacerParrafos(tablaM,cantidad);
 			document.formu.estructura.value="PARRAFOS";
-		}	
+		}
 		else{
-			hacerTabla(TablaM,cantidad);
+			hacerTabla(tablaM,cantidad);
 			document.formu.estructura.value="TABLA";
+		
 		}
 		this.parentNode.removeChild(this);
+		
 	}
-	
 	function cambiarEstructura2(){
 		menu();
-		document.body.removeChild(document.body.children[0]);
+		document.body.removeChild(document.body.children[0]);	
 	}
-
+	
 	function hacerLista(tablaMulti,row){
-		let lista = document.createElement("ul");//Acabo de crear la etiqueta ul
+		let lista=document.createElement("ul");
 		lista.id="lista";
-		lista.onclick = cambiarEstructura;
+		lista.onclick=cambiarEstructura;
 		let elemento,texto;
-		for(let i = 1; i<=row;i++){
-			let elemento = document.createElement("li");
-			texto = document.createTextNode(tablaMulti+"x"+i+"="+(tablaMulti*i));
+		for(let i=1;i<=row;i++){
+			elemento=document.createElement("li");
+			texto=document.createTextNode(tablaMulti+"x"+i+"="+(tablaMulti*i));
 			elemento.appendChild(texto);
-			lista.appendChild(elemento);//Metemos en el ul.
+			lista.appendChild(elemento);
 		}
-		let formulario = document.formu;
+		let formulario=document.formu;
 		document.body.insertBefore(lista,formulario);
 	}
 	
-	function hacerParrafo(tablaMulti,row){
-		let parra = document.createElement("div");
-		parra.id="parrafos";
-		parra.onclick = cambiarEstructura;
-		parra.style.border="1px solid black";
-		let elemento,texto;
-		for(let i = 1; i<=row;i++){
-			let elemento = document.createElement("p");
-			texto = document.createTextNode(tablaMulti+"x"+i+"="+(tablaMulti*i));
-			elemento.appendChild(texto);
-			parra.appendChild(elemento);
-		}
-		let formulario = document.formu;
-		document.body.insertBefore(parra,formulario);
-	}
 	
-	function hacerDesplegable(tablaMulti,row){
-		let sele = document.createElement("select");
-		sele.id = "desplegable";
-		sele.onchange = cambiarEstructura;
+	function hacerParrafos(tablaMulti,row){
+		let contenedor=document.createElement("div");
+		contenedor.id="parrafos";
+		contenedor.onclick=cambiarEstructura;
+		contenedor.style.border="1px solid black";
 		let elemento,texto;
-		for(let i = 1; i<=row;i++){
-			let elemento = document.createElement("option");
-			elemento.value="opcion"+i;
-			texto = document.createTextNode(tablaMulti+"x"+i+"="+(tablaMulti*i));
+		for(let i=1;i<=row;i++){
+			elemento=document.createElement("p");
+			texto=document.createTextNode(tablaMulti+"x"+i+"="+(tablaMulti*i));
 			elemento.appendChild(texto);
-			sele.appendChild(elemento);
+			contenedor.appendChild(elemento);
 		}
-		let formulario = document.formu;
-		document.body.insertBefore(sele,formulario);
-	}
+		let formulario=document.formu;
+		document.body.insertBefore(contenedor,formulario);		
+		
+	}	
+	function hacerDesplegable(tablaMulti,row){
+		let despl=document.createElement("select");
+		despl.id="desplegable";
+		despl.onchange=cambiarEstructura;
+		let elemento,texto;
+		for(let i=1;i<=row;i++){
+			elemento=document.createElement("option");
+			elemento.value="opcion"+i;
+			texto=document.createTextNode(tablaMulti+"x"+i+"="+(tablaMulti*i));
+			elemento.appendChild(texto);
+			despl.appendChild(elemento);
+		}
+		let formulario=document.formu;
+		document.body.insertBefore(despl,formulario);		
+		
+	}	
 	
 	function hacerCelda(row,cadena){
-				let celda,texto;
-				celda = document.createElement("td");
-				celda.style.border="1px solid black";
-				texto=document.createTextNode(menor+"x"+i);
-				celda.appendChild(texto);
-				row.appendChild(celda);
-				}
-	
+		let celda,texto;
+		celda=document.createElement("td");
+		celda.style.border="1px solid black";
+		texto=document.createTextNode(cadena);
+		celda.appendChild(texto);
+		row.appendChild(celda);					
+	}	
 	function hacerTabla(tablaMulti,row){
-		let tabla = document.createElement("table");
+		let tabla=document.createElement("table");
 		tabla.id="tabla";
-		tabla.onclick = cambiarEstructura;
-		tabla.style.border = "1px solid black";
-		tabla.style.borderCollapse = "collapse";
+		tabla.onclick=cambiarEstructura;
+		tabla.style.border="1px solid black";
+		tabla.style.borderCollapse="collapse";
 		let fila;
 		
 		for(let i=1;i<=row;i++)
+		{
+			fila=document.createElement("tr");
 			
-			{
-				fila = document.createElement("tr");
-				
-				hacerCelda(fila,tablaMulti+"x"+i);
-				hacerCelda(fila,"=");
-				hacerCelda(fila,tablaMulti*i);
-				tabla.appendChild(fila);
-				
-			}
-		let formulario = document.formu;			
-		document.body.insertBefore(tabla,formulario);
+			hacerCelda(fila,tablaMulti+"x"+i);
+			hacerCelda(fila,"=");
+			hacerCelda(fila,tablaMulti*i);
+			tabla.appendChild(fila);
+		}
+		let formulario=document.formu;
+		document.body.insertBefore(tabla,formulario);	
 	}
 	
 	function menu(){
-		let f = document.formu;
+		let f=document.formu;
 		let opcionElegida,modo;
-			modo=f.estructura.value;
-			let tabla,filas;
-			if(modo!="SALIR"){
-				tabla=f.tabla.value;
-				filas=f.cantidad.value;
-			}
-			
-			let radios = document.formu.estructura;
-			for(let i = 0; i < radios.length; i++){
-				radios[i].onclick=cambiarEstructura2;
-			}
+		modo=f.estructura.value;
+		let tabla,filas;
+		if(modo!="SALIR"){
+			tabla=f.tabla.value;
+			filas=f.cantidad.value;
+		}
+		
+		let radios=document.formu.estructura;
+		for(let i=0;i<radios.length;i++)
+			radios[i].onclick=cambiarEstructura2;
 
-			switch(modo){ 
-				case "LISTA":
-								hacerLista(tabla,filas);
-								//hacerEstructuraBasica(tabla,filas,"<ul>","<li>","</li>","</ul>");
-								break;
-				case "PARRAFOS":
-								hacerParrafo(tabla,filas);
-								//hacerEstructuraBasica(tabla,filas,"<div style='border:1px solid black;'>","<p>","</p>","</div>");
-								break;		
-				case "TABLA":
-								hacerTabla(tabla,filas);
-								break;
-				case "DESPLEGABLE":
-								hacerDesplegable(tabla,filas);
-								//hacerEstructuraBasica(tabla,filas,"<select>","<option>","</option>","</select>");
-								break;	
-				case "SALIR":
-								alert("Hasta luego");
-			}
-		}			
-	
+		switch(modo){
+			case "LISTA":
+							hacerLista(tabla,filas);
+							//hacerEstructuraBasica(tabla,filas,"<ul>","<li>","</li>","</ul>");
+							break;
+			case "PARRAFOS":
+							hacerParrafos(tabla,filas);
+							//hacerEstructuraBasica(tabla,filas,"<div style='border:1px solid black;'>","<p>","</p>","</div>");
+							break;		
+			case "TABLA":
+							hacerTabla(tabla,filas);
+							break;
+			case "DESPLEGABLE":
+							hacerDesplegable(tabla,filas);
+							//hacerEstructuraBasica(tabla,filas,"<select>","<option>","</option>","</select>");
+							break;	
+			case "SALIR":
+							alert("Hasta luego");
+		}
+	}
+
 	function validar(){
-		let f = document.formu;
-		if(f.tabla.value == ""){
+		let f=document.formu;
+		if(f.tabla.value==""){
 			alert("Error el campo de la tabla debe rellenarse");
 			return false;
 		}
-		
-		if(f.cantidad.value == ""){
+		if(f.cantidad.value==""){
 			alert("Error el campo de la cantidad debe rellenarse");
 			return false;
 		}
-		
-		if(f.estructura.value == ""){
+		if(f.estructura.value==""){
 			alert("Error debe marcar una estructura");
 			return false;
-		}
-		
+		}		
 		menu();
 		event.preventDefault();
 	}
 	
 	function validarNumeros(nodo,campo){
-		if(isNaN(nodo.value) || nodo.value<1 || nodo.value>10){
+		if(isNaN(nodo.value) || nodo.value<1 || nodo.value>10)
 			nodo.setCustomValidity("Error el campo "+campo+" debe ser numérico");
-		}
-		else 
+		else
 			nodo.setCustomValidity("");
 	}
 	
 	window.onload=function(){
-		let f = document.formu;
+		let f=document.formu;
 		f.onsubmit=validar;
+		
 		f.tabla.oninput=function(){
 			validarNumeros(this,"tabla");
 		};
 		f.cantidad.addEventListener("blur",function(){
-			validarNumeros(this,"cantidad");
-		});
+									validarNumeros(this,"cantidad");
+									});
 	}
 	
 	
